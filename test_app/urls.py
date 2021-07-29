10fb393a9437b24b5b821ffd60e5ba43bd94dee6
eltemucano <crosales.temuco@gmail.com>
2021-07-28 23:19:09 -0400

    Primer commit

---
+++ test_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+# En esta sección defines las redirecciones a la vista
+
+urlpatterns = [
+    path('',views.index)
+] 
---
+++ test_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,render, HttpResponse
+
+# Create your views here.
+def index(request):
+    
+    return HttpResponse ('Pagina inicial en blanco')
